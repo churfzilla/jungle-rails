7a4928fefd90f3492221bcd554a1f5600cdc8217
Chad Bruhaug <churfzilla@gmail.com>
2016-10-17 15:11:33 -0700

    added more user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,20 @@
 class User < ActiveRecord::Base
 
+
+  def strip_whitespace
+    self.email = self.email.strip unless self.email.nil?
+  end
+
+  before_validation :strip_whitespace, :only => [:email]
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false }
 
-  validates_confirmation_of :password
+  validates :password,  presence: true,
+                        confirmation: true,
+                        length: {within: 6..40}
+
 
   has_secure_password
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,33 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
+
+    let(:chad) { User.new(first_name: 'Chad', last_name: 'Bruhaug', email: 'chadbruhaug@gmail.com', password: 'password', password_confirmation: 'password' )}
+    let(:chad2) { User.new(first_name: 'Chad', last_name: 'Bruhaug', email: 'ChadBruhaug@gmail.com', password: 'password', password_confirmation: 'password' )}
+    let(:chad3) { User.new(first_name: 'Chad', last_name: 'Bruhaug', email: 'Chad3Bruhaug@gmail.com', password: 'password', password_confirmation: 'password' )}
+    let(:emailtest) { User.new(first_name: 'Email', last_name: 'Test', email: 'test@test.com', password: 'test', password_confirmation: 'test')}
+
+    it 'should be a valid test subject' do
+        expect(chad).to(be_valid)
+        # raise Error unless user.valid?
+    end
+
+    xit 'should not be a valid test subject' do
+        expect(chad2).to_not(be_valid)
+        # raise Error unless user.valid?
+    end
+
+    xit 'should have a unique email' do
+      chad2.save
+      chad.save
+      expect(chad).to_not(be_valid)
+    end
+
+    it 'Should have a password longer than 6 chars' do
+      emailtest.save
+      expect(emailtest).to_not(be_valid)
+    end
+
+  end
+
 end
