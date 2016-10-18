2b1bbc6234cf2a0773ed3659276274f16ed2d30c
Chad Bruhaug <churfzilla@gmail.com>
2016-10-18 12:14:14 -0700

    product testing

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    1.times do |n|
+      @category.products.create!(
+        name:  'Test',
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the product" do
+    # ACT
+    visit root_path
+    page.find('.product').find_link('Test').click
+
+    # DEBUG
+    expect(page).to have_css 'article.product'
+
+    # VERIFY
+    save_screenshot
+
+  end
+end
